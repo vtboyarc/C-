7120e327e379063e2de058ad9a5cfbe7a08c8e5b
vtboyarc <adamrcarter@gmail.com>
2021-01-21 19:02:39 -0600

    c plus plus boi

--- helloWorld2/hello2.cpp
+++ helloWorld2/hello2.cpp
@@ -1,6 +1,8 @@
 /* hello.cpp */
 #include <iostream>
 using namespace std;
+// to run, do terminal, run build task. in cli, in the folder the file is in
+// do ./hello2 - it is the name of the file
 int main()
 {
     cout << "Please enter your name: ";
